51aa6e84a049850fa769b6a863f42aff69aa0235
uriab <bmw.love1@gmail.cm>
2017-12-28 03:01:13 +0100

    4\5

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ $botToken = "485446580:AAH96fgUtkDjsd1fN-XHFVitCryoOgC2ChE";
 $website = 	"https://api.telegram.org/bot".$botToken;
 
 
-$update = file_get_contents("php://input"));
+$update = file_get_contents("php://input");
 $updateArray = json_decode($update, true);
 $chatId = $updateArray["result"][0]["chat"]["id"];
 file_get_contents($website."/sendmessage?chat_id=".$chatId."&text=test");
