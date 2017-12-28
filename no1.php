80cede1b63423510547501893bad3b705c95f686
uriab <bmw.love1@gmail.cm>
2017-12-28 02:04:57 +0100

    clean

--- no1.php
+++ no1.php
@@ -1,1 +1,33 @@
-curl -F "url=https://raw.githubusercontent.com/uriab/paroos/master/test.php" -F https://api.telegram.org/bot485446580:AAH96fgUtkDjsd1fN-XHFVitCryoOgC2ChE/setWebhook
+
+//$update = json_decode(file_get_contents('php://input'));
+
+//$chatid = $update->message->chat->id;
+//$message_id = $update->message->message_id;;
+//$text = $update->message-text;
+
+/*
+function makeCurl($method,$datas,$api){
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,"https://api.telegram.org/bot{$api}/{$method}");
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS,
+		http_build_quuery($datas));
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+	$server_output = curl_exec ($ch);
+	curl_close ($ch);
+	return json_decode($server_output);
+
+}
+
+makeCurl(
+					'sendmessage',
+					[
+							'chat_id'=>$chatid,
+							'text'=>$text,
+							'reply_to_message_id'=>$message_id,
+							'parse_mode'=>'HTML'
+					]
+					,
+					'485446580:AAH96fgUtkDjsd1fN-XHFVitCryoOgC2ChE'
+				);
+*/
